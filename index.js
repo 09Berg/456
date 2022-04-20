2b2c828e52b058686d100f182fa5b5d6e908a758
09Berg <103394803+09Berg@users.noreply.github.com>
2022-04-20 11:00:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
             var firstnum = document.getElementById("firstnum").value;
             var lastnum = document.getElementById("lastnum").value;
             var sum =  Number(firstnum) + Number(lastnum) 
-            var reg = /\d/;
+            var reg = /^\d$/;
             var flag1 = reg.test(firstnum);
             var flag2 = reg.test(lastnum);
             if (!flag1 || !flag2) {
